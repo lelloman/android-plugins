a58ff35c5596d4fbbac7c3720c9affa1707133cb
lelloman <cerasuolodomenico@gmail.com>
2017-08-03 22:20:32 +0200

    plugin a returns view from inflated xml

--- PluginA/app/src/main/java/com/lelloman/plugina/MyViewCreator.java
+++ PluginA/app/src/main/java/com/lelloman/plugina/MyViewCreator.java
@@ -1,9 +1,8 @@
 package com.lelloman.plugina;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.lelloman.androidplugins.AbstractViewCreator;
 
@@ -16,9 +15,6 @@ public class MyViewCreator extends AbstractViewCreator {
 
 	@Override
 	public View createView() {
-		TextView textView = new TextView(getContext());
-		textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-		textView.setText("Hello Plugin A");
-		return textView;
+		return LayoutInflater.from(getContext()).inflate(R.layout.view_hello_plugin_a, null);
 	}
 }
