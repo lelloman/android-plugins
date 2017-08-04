3f1286bf3aa6bd8c5f8985353ab47241a9d6d875
lelloman <cerasuolodomenico@gmail.com>
2017-08-04 19:50:05 +0200

    AbstractPlugin

--- PluginA/app/src/main/java/com/lelloman/plugina/Plugin.java
+++ PluginA/app/src/main/java/com/lelloman/plugina/Plugin.java
@@ -4,17 +4,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 
-import com.lelloman.androidplugins.AbstractViewCreator;
+import com.lelloman.androidplugins.AbstractPlugin;
 
 
-public class MyViewCreator extends AbstractViewCreator {
+public class Plugin extends AbstractPlugin {
 
-	public MyViewCreator(Context context) {
+	public Plugin(Context context) {
 		super(context);
 	}
 
 	@Override
-	public View createView() {
+	public View createHomeCard() {
 		return LayoutInflater.from(getContext()).inflate(R.layout.view_hello_plugin_a, null);
 	}
 }
--- PluginA/app/src/main/java/com/lelloman/plugina/PluginFrameLayout.java
+++ PluginA/app/src/main/java/com/lelloman/plugina/PluginFrameLayout.java
@@ -1,7 +1,19 @@
 package com.lelloman.plugina;
 
-import FarmeLayout;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
 
-public class PluginFrameLayout extends FarmeLayout {
+public class PluginFrameLayout extends FrameLayout {
+
+	public PluginFrameLayout(@NonNull Context context) {
+		super(context);
+	}
+
+	public PluginFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+	}
 }
